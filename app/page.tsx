c2e2ba02ad29729d60e190fecb040d82e289a07e
clieantapp-crypto <clieantapp@gmail.com>
2025-11-30 04:01:02 +0300

    ssss

--- app/page.tsx
+++ app/page.tsx
@@ -21,8 +21,8 @@ export default function LoginPage() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [formData, setFormData] = useState<LoginFormData>({
-    email: "mes@jo.com",
-    password: "mes@jo.com",
+    email: "me199@admin.xo",
+    password: "me199@admin.xo",
   });
 
   const router = useRouter();
